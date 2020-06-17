88e8cec0a247904ae391f2a4000bc0194bfdebf4
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2020-06-17 15:58:57 -0400

    Testing for mobile

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -4,24 +4,24 @@ import { isMobile } from 'react-device-detect';
 
 class TopNav extends React.Component {
     getNav() {
-        if (isMobile) {
-            return(
-                <header className="header-down">
-                    <div className="container-left">
-                        <a href="/">
-                            <div className="logo">
-                                <p>MB</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="container-right">
-                        <nav>
-
-                        </nav>
-                    </div>
-                </header>
-            );
-        }
+        // if (isMobile) {
+        //     return(
+        //         <header className="header-down">
+        //             <div className="container-left">
+        //                 <a href="/">
+        //                     <div className="logo">
+        //                         <p>MB</p>
+        //                     </div>
+        //                 </a>
+        //             </div>
+        //             <div className="container-right">
+        //                 <nav>
+        //
+        //                 </nav>
+        //             </div>
+        //         </header>
+        //     );
+        // }
         return (<header className="header-down">
             <div className="container-left">
                 <a href="/">
