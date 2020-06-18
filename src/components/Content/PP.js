71fa418edbbd1d522911e9a6b84ab2a26119b987
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2020-06-17 21:08:22 -0400

    Fixed weird disappearing bug

--- src/components/Content/PP.js
+++ src/components/Content/PP.js
@@ -4,30 +4,19 @@ import {isMobile} from 'react-device-detect';
 class PP extends React.Component {
     getPP () {
         if (isMobile) {
-            return;
+            return(
+                <div id="personal-projects" style={{visibility: 'hidden', paddingTop: '50px'}}>
+                    <div className="content-div-mobile" style={{marginTop: '50px'}}>
+                        <p className="page-title" style={{fontSize: '40px'}}>Personal Projects</p>
+                        <div className="closing-bar" style={{marginTop: '50px'}}></div>
+                    </div>
+                </div>
+            );
         }
         return(
             <div id="personal-projects" style={{visibility: 'hidden', paddingTop: '50px'}}>
                 <div className="content-div-mobile" style={{marginTop: '50px'}}>
                     <p className="page-title" style={{fontSize: '40px'}}>Personal Projects</p>
-                    <div className="row">
-                        <div className="col-md-8" style={{color: 'darkgrey', fontSize: '14px'}}>
-                        </div>
-                        <div className="col-md-4" style={{padding: '0'}}>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4" style={{padding: '0'}}>
-                        </div>
-                        <div className="col-md-8" style={{color: 'darkgrey', fontSize: '14px'}}>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-8" style={{color: 'darkgrey', fontSize: '14px'}}>
-                        </div>
-                        <div className="col-md-4" style={{padding: '0'}}>
-                        </div>
-                    </div>
                     <div className="closing-bar" style={{marginTop: '50px'}}></div>
                 </div>
             </div>
